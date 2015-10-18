39d8507dbc25fa1de116b2bcbb8625c2f64ddffb
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2015-10-18 12:21:58 -0700

    Commands: Initial `status` implementation.
    Inclues wiring up the WinkClient, logging in from a manifest, and
    getting current device state.

---
+++ cmd/status.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var path = require('path')
+var WinkClient = require('../lib/wink')
+
+/**
+ * Fulfills the `status` command. See README and `man/status` for more
+ * information.
+ */
+function status(argv, options, loader) {
+  var manifest = require(path.resolve(process.cwd(), argv[0]))
+
+  return WinkClient
+    .login(manifest.account)
+    .then(function (client) {
+      return client.getDevices()
+    })
+}
+
+/*!
+ * Export `status`.
+ */
+module.exports = status
